951673dda0982ad42345339c39f8e6258dfb3054
Johannes Schmatz <johannes_schmatz@gmx.de>
2022-09-06 00:31:46 +0200

    remove old runtime impl

--- src/main/java/de/skyrising/litefabric/impl/fs/FileAccess.java
+++
@@ -1,9 +0,0 @@
-package de.skyrising.litefabric.impl.fs;
-
-import java.io.IOException;
-import java.nio.file.attribute.BasicFileAttributes;
-
-public interface FileAccess {
-    byte[] getBytes(String path) throws IOException;
-    BasicFileAttributes getAttributes(String path) throws IOException;
-}
